3d19896d378283384619cf3da7a0fe5889e60907
Louis Roché <louis@louisroche.net>
2014-04-03 01:06:21 +0800

    add switch button

--- temperature.ml
+++ temperature.ml
@@ -1,7 +1,5 @@
 module Html = Dom_html
 
-let (>>=) = Lwt.bind
-
 let debug_mode = true
 
 let debug fmt =
@@ -18,7 +16,27 @@ let update id value =
       (fun () -> assert false) in
   Js.Unsafe.set node "value" value
 
-let handler from_id to_id element _ =
+let convert_F_to_C f =
+  (f -. 32.0) /. 1.8
+
+let convert_C_to_F c =
+  (c *. 1.8) +. 32.0
+
+let switch _ _ =
+  let d = Html.document in
+  let celsius =
+    Js.Opt.get (d##getElementById(Js.string "celsius"))
+      (fun () -> assert false) in
+  let fahrenheit =
+    Js.Opt.get (d##getElementById(Js.string "fahrenheit"))
+      (fun () -> assert false) in
+  let celsius_value: Js.js_string Js.t = Js.Unsafe.get celsius "value" in
+  let fahrenheit_value: Js.js_string Js.t = Js.Unsafe.get fahrenheit "value" in
+  Js.Unsafe.set celsius "value" fahrenheit_value;
+  Js.Unsafe.set fahrenheit "value"celsius_value;
+  Lwt.return ()
+
+let handler from_id to_id convert _ _ =
   let d = Html.document in
   let from =
     Js.Opt.get (d##getElementById(Js.string from_id))
@@ -28,17 +46,22 @@ let handler from_id to_id element _ =
       debug "oups"
     else
       let t = Js.parseFloat temp in
+      let _ = update to_id (convert t) in
       Firebug.console##log(t);
       debug "ok"
   in
-  let _ = update to_id temp in
   Lwt.return ()
 
-let onload _ =
+let load _ =
   let d = Html.document in
   let body =
-    Js.Opt.get (d##getElementById(Js.string "wiki_demo"))
+    Js.Opt.get (d##getElementById(Js.string "temperature"))
       (fun () -> assert false) in
+  let button = Html.createInput
+      ?_type:(Some (Js.string "button"))
+      ?name:(Some (Js.string "switch"))
+      d in
+  let () = button##value <- Js.string "switch values" in
   let celsius = Html.createInput
       ?_type:(Some (Js.string "number"))
       d in
@@ -52,10 +75,29 @@ let onload _ =
   fahrenheit##id <- Js.string "fahrenheit";
   fahrenheit##defaultValue <- Js.string "32";
   Dom.appendChild body celsius;
+  Dom.appendChild body (Html.createBr d);
+  Dom.appendChild body button;
+  Dom.appendChild body (Html.createBr d);
   Dom.appendChild body fahrenheit;
-  Lwt.ignore_result (Lwt_js_events.inputs celsius (handler "celsius" "fahrenheit"));
-  Lwt.ignore_result (Lwt_js_events.inputs fahrenheit (handler "fahrenheit" "celsius"));
+  Lwt.ignore_result (Lwt_js_events.inputs
+                       celsius
+                       (handler
+                          "celsius"
+                          "fahrenheit"
+                          convert_C_to_F
+                       )
+                    );
+  Lwt.ignore_result (Lwt_js_events.inputs
+                       fahrenheit
+                       (handler
+                          "fahrenheit"
+                          "celsius"
+                          convert_F_to_C
+                       )
+                    );
+  Lwt.ignore_result (Lwt_js_events.clicks button switch);
+  celsius##focus ();
   Js._false
 
 let _ =
-  Html.window##onload <- Html.handler onload
+  Html.window##onload <- Html.handler load
